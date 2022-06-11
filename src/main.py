dddd3fcfdebc60b39e2fcd8221cb4bdbddf8a427
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-06-12 04:14:44 +0530

    Add main.py

---
+++ src/main.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()  # OR
+    # doctest.run_docstring_examples(StudentAccount, globals(), name='HW2',verbose=False) # replace Course for the class name you want to test
